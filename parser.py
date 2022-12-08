06fbc4c2d0949f49610cb23599e851f3903fee67
Théo <theo.moulaert1@gmail.com>
2022-12-08 17:42:21 +0100

    Basics of Fireflux

---
+++ parser.py
@@ -0,0 +1,16 @@
+class Parser :  
+
+    def __init__(self):
+        self.parse_success = False 
+        self.rules = []
+    
+    #Connect to the interface
+    def parse(self, xml):
+        pass
+    
+    def get_rules(self):
+        return self.rules
+
+    #Display the informations
+    def __str__(self):
+        pass
