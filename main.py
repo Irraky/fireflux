072db53cef8964f325a581de00116153f4a11f62
Virgiel <>
2023-02-10 14:01:07 +0100

    Refactor project

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-from readCredentials import ReadCredentials
-from pfsenseConnector import PfsenseConnector
-from pfsenseParser import PfsenseParser
-import pandas as pd
+import pfsense
 
-# GET CREDENTIALS
-reader = ReadCredentials("./resources/credentials.txt")
-reader.read()
+url = "http://172.16.143.2/"
+username = "admin"
+password = "pfsense"
 
-# print(reader.get_success())
-credentials = reader.get_credentials()
-
-# CONNECT TO THE FIREWALL
-pfsense = PfsenseConnector(credentials[0])
-pfsense.connect()
-
-# print(pfsense.get_login_success())
-# print(pfsense)
-
-# RETRIVE BACKUP FILE
-backup = pfsense.retrieve()
-
-# PARSE THE BACKUP XML
-parser = PfsenseParser()
-parser.parse(backup)
-
-# print(parser.get_success())
-print(parser.get_rules()[0])
-
-# BUILD THE MATRIX
-pd.DataFrame(parser.get_rules_list()).to_excel('output.xlsx', header=False, index=False)
+data = pfsense.extract(url, username, password)
+print(data)
